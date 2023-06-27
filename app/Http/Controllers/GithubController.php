d0807d34393a6e0971900285494ba09b1c90f2a0
Firman Erza <firman.erzap@gmail.com>
2023-06-27 17:54:37 +0700

    update

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -9,11 +9,42 @@ class GithubController extends Controller
     //
     public function index()
     {
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', ' https://api.github.com/repositories');
+        $github_token = env('GITHUB_TOKEN');
+        $ch = curl_init();
+        $headers = array(
+            'Accept: application/vnd.github+json',
+            "Authorization: Bearer $github_token" ,
+            "X-GitHub-Api-Version: 2022-11-28",
+            "User-Agent: ferzap"
+        );
 
-        $response = json_decode($response->getBody()->getContents());
+        // print_r($ch);die;
+        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/user/repos');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST , "GET");
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        return $response;
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        $results = json_decode($response);
+
+        $arrRepo = [];
+        foreach ($results as $key => $value) {
+            $repo = array(
+                'id' => $value->id,
+                "name" => $value->name,
+                "fullName" => $value->full_name,
+                "link" => $value->html_url
+            );
+            $arrRepo[] = $repo;
+        }
+
+        return response()->json([
+            "data" => $arrRepo
+        ]);
     }
 }
